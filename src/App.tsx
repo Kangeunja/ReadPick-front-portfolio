c7c95e37ae353b2f73606ecd00b0a91d09f646cf
EunjaKang <dnqls2414@naver.com>
2026-07-10 13:48:19 +0900

    feat: 무한루프 429에러로 인한 옵션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import BrowserRouterDom from './router';
 
 import './index.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const App = () => {
   return (
